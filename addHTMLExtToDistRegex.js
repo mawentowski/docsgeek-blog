4d1954157591d719cd51d38a06f35ae09b0fbc2e
mawentowski <mawentowskigit@gmail.com>
2023-10-07 22:11:46 +0700

    created a test script to add HTML extensions

--- addHTMLExtToDistRegex.js
+++ addHTMLExtToDistRegex.js
@@ -1,28 +1,38 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-// Define the regex pattern for matching href attributes that don't end with .css.
+// The following adds HTML extensions to URLs so they local prod build can be tested. The server is already equipped to interpet the .html extension and not include it.
 const regexReplacements = [
-  // ensure that all hrefs start with a '/blog'
-  { pattern: /href="\/([^"]+)"/g, replacement: 'href="/blog/$1"' },
-  // get rid of repeats: /blog/blog/
-  { pattern: /href="\/blog\/blog\//g, replacement: 'href="/blog/' },
-  // Ensure that all srcs start with '/blog
-  { pattern: /src="\/([^"]+)"/g, replacement: 'src="/blog/$1"' },
-  // get rid repeats: /blog/blog/
-  { pattern: /src="\/blog\/blog\//g, replacement: 'src="/blog/' },
+  // add .html to pages
+  {
+    pattern: /href="\/blog\/([^"]+)"/g,
+    replacement: 'href="/blog/$1.html"',
+  },
+  // remove instances where '.html' was added to '.css' files
+  { pattern: /.css.html/g, replacement: ".css" },
+  // remove instances where '.html' was added to '.svg' files
+  { pattern: /.svg.html/g, replacement: ".svg" },
+  // remove instances where '.html' was added to '.svg' files
+  { pattern: /.png.html/g, replacement: ".png" },
+  // remove instances where '.html' was added to '.xml' files
+  { pattern: /.xml.html/g, replacement: ".xml" },
+  // Add .html to post routes on the search page
+  {
+    pattern: /href:`\/blog\/posts\/\${Du\(u.data\)}/g,
+    replacement: "href:`/blog/posts/$${Du(u.data)}.html",
+  },
 ];
 
 // Function to perform regex replacements in a given file.
 function performRegexReplacements(filePath) {
   // Read the content of the file.
-  let content = fs.readFileSync(filePath, 'utf8');
-  
+  let content = fs.readFileSync(filePath, "utf8");
+
   // Apply each regex replacement defined in regexReplacements.
-  regexReplacements.forEach(replacement => {
+  regexReplacements.forEach((replacement) => {
     content = content.replace(replacement.pattern, replacement.replacement);
   });
-  
+
   // Write the modified content back to the file.
   fs.writeFileSync(filePath, content);
 }
@@ -30,7 +40,7 @@ function performRegexReplacements(filePath) {
 // Recursive function to process directories and files.
 function processDirectory(directoryPath) {
   const files = fs.readdirSync(directoryPath);
-  files.forEach(file => {
+  files.forEach((file) => {
     const filePath = path.join(directoryPath, file);
     if (fs.statSync(filePath).isDirectory()) {
       // Continue processing subdirectories.
@@ -38,7 +48,7 @@ function processDirectory(directoryPath) {
     } else {
       // Check if the file has a supported extension (.html or .js).
       const ext = path.extname(filePath).toLowerCase();
-      if (ext === '.html' || ext === '.js') {
+      if (ext === ".html" || ext === ".js") {
         // Perform replacements in .html and .js files.
         performRegexReplacements(filePath);
       }
@@ -47,7 +57,7 @@ function processDirectory(directoryPath) {
 }
 
 // Define the root directory to start processing.
-const rootDirectory = path.join(__dirname, 'dist');
+const rootDirectory = path.join(__dirname, "dist");
 
 // Start processing directories and files.
 processDirectory(rootDirectory);
